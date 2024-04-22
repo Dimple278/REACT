9b1c795295537771209ef0dd1c50df591f0a2402
Dimple <dsaraogi2000@gmail.com>
2024-04-22 18:53:37 +0545

    Structured files

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const IMG_URL =
+  "https://www.shutterstock.com/image-photo/lopburi-thailand-october-272020-kfc-260nw-1841921497.jpg";
+export const LOGO_URL =
+  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
