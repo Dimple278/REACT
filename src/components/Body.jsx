f03c4b917fa4c2d01d06e20ea118db8d57cb553a
Dimple <dsaraogi2000@gmail.com>
2024-04-24 14:53:09 +0545

    implement search bar

---
+++ src/components/Body.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredListOfRestauants, setFilteredListOfRestaurants] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  const handleFilter = () => {
+    const filteredList = listOfRestaurants.filter(
+      (res) => res.info.avgRating > 4.2
+    );
+
+    setListOfRestaurants(filteredList);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5/?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    //Optional Chaining
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  console.log("rendered body");
+  //conditional rendering
+  return listOfRestaurants.length == 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="searchBox"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="btn"
+            onClick={() => {
+              let filteredRestaurants = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredListOfRestaurants(filteredRestaurants);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button className="btn" onClick={handleFilter}>
+          Top Rated restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredListOfRestauants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
