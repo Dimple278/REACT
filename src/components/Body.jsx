98f865055e9b64219e09487019c9552a1e26c50a
dimple <dsaraogi2000@gmail.com>
2024-04-26 22:30:08 +0545

    create useOnlineStatus hook

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
@@ -36,6 +37,15 @@ const Body = () => {
     );
   };
   console.log("rendered body");
+
+  const onlineStatus = useOnlineStatus();
+  if (onlineStatus === false)
+    return (
+      <h1>
+        Looks like you're offline!! Please check your internet connection;
+      </h1>
+    );
+
   //conditional rendering
   return listOfRestaurants.length == 0 ? (
     <Shimmer />
